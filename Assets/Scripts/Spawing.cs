2a5e77bfac9416625975a5c24d6c96d836a948ce
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-14 23:15:26 +0100

    Added effect at level start

---
+++ Assets/Scripts/Spawing.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawing : MonoBehaviour
+{
+    ParticleSystem system;
+    bool stopped;
+    public GameObject player;
+    bool enabled = true;
+
+    void Start()
+    {
+        system = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (enabled)
+        {
+            if (system.startSpeed > -12f && !stopped)
+            {
+                system.startSpeed -= Time.deltaTime * 2;
+                system.emissionRate += Time.deltaTime;
+            }
+            else
+            {
+                system.startSpeed = 0;
+                system.emissionRate = 0;
+                stopped = true;
+
+                system.startSpeed = 5;
+                system.startColor = Color.black;
+                system.Emit(40);
+
+                GameObject playera = Instantiate(player, transform.position, Quaternion.identity) as GameObject;
+                Camera.main.GetComponent<CameraFollow>().player = playera.transform;
+                enabled = false;
+            }
+        }
+    }
+}
