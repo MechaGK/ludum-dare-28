e5904a93b9265de24ced85e1e1f1ef82fd59a467
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-14 16:33:53 +0100

    Watch script for making "viser" rotate to TimeControl deltaTime

---
+++ Assets/Watch.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Watch : MonoBehaviour
+{
+    public Transform viser;
+
+    void Update()
+    {
+        float rotZ = viser.eulerAngles.z;
+        viser.rotation = Quaternion.Euler(0, 0, rotZ - 360 * TimeControl.DeltaTime);
+    }
+}
