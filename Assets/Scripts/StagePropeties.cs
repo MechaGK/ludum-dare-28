e35a98d23b7acffe47caae89bef0f561fe3b678a
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-14 21:55:40 +0100

    StagePropeties script for general level variables like size

---
+++ Assets/Scripts/StagePropeties.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StagePropeties : MonoBehaviour
+{
+    public static StagePropeties BLAH;
+    public Rect stageDimensions;    //X and Y is left, bottom
+
+    public Rect StageDimensions
+    {
+        get
+        {
+            return stageDimensions;
+        }
+    }
+
+    void Awake()
+    {
+        BLAH = this;
+    }
+}
