1cb7fb70ef415f4636c3a653d9d4215dd9c8744f
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-14 21:56:23 +0100

    Camera now follows player within level dimensions

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    Rect allowedArea;
+
+    void Start()
+    {
+        Rect StageDim = StagePropeties.BLAH.StageDimensions;
+
+        allowedArea = new Rect(
+            StageDim.x + camera.orthographicSize * (Screen.width / Screen.height),
+            StageDim.y + camera.orthographicSize,
+            StageDim.x + StageDim.width - (camera.orthographicSize * (Screen.width / Screen.height)),
+            StageDim.y + StageDim.height - (camera.orthographicSize * (Screen.width / Screen.height)));
+    }
+
+    void Update()
+    {
+        Vector3 newPos = player.position;
+
+        newPos.x = Mathf.Clamp(newPos.x, allowedArea.x, allowedArea.width);
+        newPos.y = Mathf.Clamp(newPos.y, allowedArea.height, allowedArea.y);
+        newPos.z = -10;
+
+        transform.position = newPos;
+    }
+}
