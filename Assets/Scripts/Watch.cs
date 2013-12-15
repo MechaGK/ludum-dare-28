f4ed61433dc18520b6a1d47c676690f0084c6d36
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-15 01:03:34 +0100

    Made watch stop going after a "second"

--- Assets/Scripts/Watch.cs
+++ Assets/Scripts/Watch.cs
@@ -4,10 +4,19 @@ using System.Collections;
 public class Watch : MonoBehaviour
 {
     public Transform viser;
+    float timer;
 
     void Update()
     {
-        float rotZ = viser.eulerAngles.z;
-        viser.rotation = Quaternion.Euler(0, 0, rotZ - 360 * TimeControl.DeltaTime);
+        if (timer < 1f)
+        {
+            float rotZ = viser.eulerAngles.z;
+            viser.rotation = Quaternion.Euler(0, 0, rotZ - 360 * TimeControl.DeltaTime);
+            timer += TimeControl.DeltaTime;
+        }
+        else
+        {
+            viser.rotation = Quaternion.Euler(Vector3.zero);
+        }
     }
 }
