4460773b82dadab2aa16da71a7a09bbcb5a109cd
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-15 01:04:14 +0100

    Redone goal apperence and added an "activated" sprite and some fading

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Goal : MonoBehaviour
+{
+    public Sprite activated;
+    bool isActivated = false;
+
+    void Update()
+    {
+        if (isActivated)
+        {
+            Color color = GetComponent<SpriteRenderer>().color;
+            color.a -= Time.deltaTime;
+            GetComponent<SpriteRenderer>().color = color;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            GetComponent<SpriteRenderer>().sprite = activated;
+            isActivated = true;
+        }
+    }
+}
