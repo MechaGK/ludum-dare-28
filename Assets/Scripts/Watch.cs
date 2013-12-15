842f65c6449e75dd181b6d9d9542acc6e22aec42
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-15 01:59:52 +0100

    Watch now is a watch and follow the players time

--- Assets/Scripts/Watch.cs
+++ Assets/Scripts/Watch.cs
@@ -4,19 +4,23 @@ using System.Collections;
 public class Watch : MonoBehaviour
 {
     public Transform viser;
+    public PlayerControl player;
     float timer;
 
     void Update()
     {
-        if (timer < 1f)
+        if (player != null)
         {
-            float rotZ = viser.eulerAngles.z;
-            viser.rotation = Quaternion.Euler(0, 0, rotZ - 360 * TimeControl.DeltaTime);
-            timer += TimeControl.DeltaTime;
-        }
-        else
-        {
-            viser.rotation = Quaternion.Euler(Vector3.zero);
+            if (player.timeSinceSpawn < 1f)
+            {
+                float rotZ = viser.eulerAngles.z;
+                viser.rotation = Quaternion.Euler(0, 0, rotZ - 360 * TimeControl.DeltaTime);
+                timer += TimeControl.DeltaTime;
+            }
+            else
+            {
+                viser.rotation = Quaternion.Euler(Vector3.zero);
+            }
         }
     }
 }
