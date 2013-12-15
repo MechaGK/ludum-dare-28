7db18441df5b9557c9c64222bba5b60c90104494
Signe Rhea Grau Kristensen <signegrau@protonmail.com>
2013-12-15 03:15:32 +0100

    Moved movement to fixedupdate and added hold-button-to-jump-higher

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -13,11 +13,14 @@ public class PlayerControl : MonoBehaviour
     bool controlable = true;
     bool respawing = false;
     public float timeSinceSpawn = 0;
+    float jumpTimer = 0;
+    float h;
+    int ticks;
 
     void Start()
     {
         Sprite sprite = GetComponent<SpriteRenderer>().sprite;
-        spriteSize = new Vector2(sprite.bounds.max.x * 2, sprite.bounds.max.y * 2);
+        spriteSize = new Vector2(sprite.bounds.max.x, sprite.bounds.max.y);
         spawn = StagePropeties.BLAH.spawn.position;
 
         Camera.main.transform.FindChild("Watch").GetComponent<Watch>().player = this;
@@ -27,29 +30,13 @@ public class PlayerControl : MonoBehaviour
     {
         if (controlable)
         {
-            float h = Input.GetAxis("Horizontal");
-
             Vector2 pos = transform.position;
-            Debug.DrawLine(pos, pos + (Vector2.right * (spriteSize.x / 2) * Mathf.Sign(h)));
-            if (!Physics2D.Linecast(pos, pos + (Vector2.right * ((spriteSize.x / 2) + 0.02f) * Mathf.Sign(h)), 1 << LayerMask.NameToLayer("Solid")))
-            {
-                rigidbody2D.velocity = new Vector3(h * speed * Time.deltaTime, rigidbody2D.velocity.y, 0);
-            }
 
-            if (Input.GetButtonDown("Jump") && grounded)
-            {
-                jump = true;
-                rigidbody2D.AddForce(Vector2.up * 30);
-            }
-
-            if (Physics2D.Linecast(pos, pos - Vector2.up * ((spriteSize.y / 2) + 0.1f), 1 << LayerMask.NameToLayer("Solid")))
+            if (Physics2D.OverlapArea(pos + Vector2.right * (spriteSize.x - 0.02f),
+                pos - Vector2.up * ((spriteSize.y) + 0.02f) - Vector2.right * (spriteSize.x - 0.02f),
+                (1 << LayerMask.NameToLayer("Platforms")) | (1 << LayerMask.NameToLayer("Floors"))))
             {
                 grounded = true;
-            }
-            else if (Physics2D.Linecast(pos, pos - Vector2.up * ((spriteSize.y / 2) + 0.1f), 1 << LayerMask.NameToLayer("MovingSolid")))
-            {
-                grounded = true;
-
             }
             else
             {
@@ -71,10 +58,47 @@ public class PlayerControl : MonoBehaviour
             }
 
             timeSinceSpawn += TimeControl.DeltaTime;
+            print(timeSinceSpawn);
 
-            if (timeSinceSpawn >= 0f)
+            if (timeSinceSpawn >= 1f)
             {
-                Die();
+                StartCoroutine(Die());
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (controlable)
+        {
+            h = Input.GetAxis("Horizontal");
+            Vector2 pos = transform.position;
+
+            if (!Physics2D.OverlapArea(pos + Vector2.up * (spriteSize.y - 0.001f),
+                pos + (Vector2.right * (spriteSize.x + 0.02f) * Mathf.Sign(h)) - Vector2.up * (spriteSize.y - 0.001f),
+                (1 << LayerMask.NameToLayer("Platforms")) | (1 << LayerMask.NameToLayer("Wall"))))
+            {
+                rigidbody2D.velocity = new Vector3(h * speed * Time.deltaTime, rigidbody2D.velocity.y, 0);
+            }
+
+            if (Input.GetButton("Jump"))
+            {
+                if (grounded)
+                {
+                    jump = true;
+                    rigidbody2D.AddForce(Vector2.up * 24);
+                    jumpTimer = 0;
+                    ticks = 0;
+                }
+                else if (jump && ticks < 6)
+                {
+                    rigidbody2D.AddForce(Vector2.up * 3.5f);
+                    ticks += 1;
+                }
+            }
+            else
+            {
+                jump = false;
             }
         }
     }
@@ -96,6 +120,7 @@ public class PlayerControl : MonoBehaviour
         {
             respawing = true;
 
+            GetComponent<SpriteRenderer>().enabled = false;
             TimeControl.timeModifier = 1;
             float timer = 0;
             controlable = false;
@@ -117,6 +142,7 @@ public class PlayerControl : MonoBehaviour
             controlable = true;
             timeSinceSpawn = 0f;
             TimeControl.timeModifier = 0f;
+            GetComponent<SpriteRenderer>().enabled = true;
 
             respawing = false;
         }
